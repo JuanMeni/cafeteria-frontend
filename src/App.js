7b7547b32e29961abdc15b690238aa053955a85e
Juan Menichetti <jpmenichetti13@gmail.com>
2022-08-22 10:04:05 -0300

    se agrego el maquetado de la pagina para crear producto

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/views/Home';
-import AdministrarProductos from './components/views/AdministrarProducto';
-import CrearProducto from './components/views/CrearProducto';
-import EditarProducto from './components/views/EditarProducto';
+import AdministrarProductos from './components/views/productos/AdministrarProducto';
+import CrearProducto from './components/views/productos/CrearProducto';
+import EditarProducto from './components/views/productos/EditarProducto';
 import Error404 from './components/views/Error404';
 import Menu from './components/common/Menu';
 import Footer from './components/common/Footer';
