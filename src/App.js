8179c91edcb07dff39c45fb9d9537ea8d4c7bf22
Juan Menichetti <jpmenichetti13@gmail.com>
2022-09-18 21:03:46 -0300

    agregamos las tareas pendientes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import EditarProducto from './components/views/productos/EditarProducto';
 import Error404 from './components/views/Error404';
 import Menu from './components/common/Menu';
 import Footer from './components/common/Footer';
+import DetalleProducto from './components/views/DetalleProducto';
 
 function App() {
   return (
@@ -17,8 +18,9 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home></Home>}></Route>
         <Route exact path='/administrar' element={<AdministrarProductos></AdministrarProductos>}></Route>
-        <Route exact path='/administrar/crear' element={<CrearProducto></CrearProducto>}></Route>
+        <Route exact path='/administrar/crear' element={<CrearProducto></CrearProducto>} ></Route>
         <Route exact path='/administrar/editar/:id' element={<EditarProducto></EditarProducto>}></Route>
+        <Route exact path='/administrar/producto/detalle/:id' element={<DetalleProducto></DetalleProducto>}></Route>
         <Route path='*' element={<Error404></Error404>}></Route>
       </Routes>
       <Footer></Footer>
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import {Link, NavLink} from 'react-router-dom';
+import {Navbar, Nav, Container} from 'react-bootstrap'
+import {NavLink, Link} from 'react-router-dom';
 
 const Menu = () => {
     return (
-        <Navbar bg="danger" variant="dark" expand="lg">
+        <Navbar bg="danger" variant='dark' expand="lg">
         <Container>
           <Navbar.Brand as={Link} to='/'>Cafeteria</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <NavLink end to='/' className='nav-item nav-link'>Inicio</NavLink>
-                <NavLink end to='/administrar' className='nav-item nav-link'>Administrar</NavLink>
-                <NavLink end to='/administrar/producto/crear' className='nav-item nav-link'>Crear</NavLink>
+              <NavLink to='/' className='nav-item nav-link'>Inicio</NavLink>
+              <NavLink end to='/administrar' className='nav-item nav-link'>Administrar</NavLink>
+              <NavLink end to='/administrar/crear' className='nav-item nav-link'>Crear</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
